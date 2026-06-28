02fe47456780b701197a98b8369f926247a4da89
RamonMartinez01 <martinezcjr1@gmail.com>
2026-06-28 17:24:51 -0600

    security(api): implement API key middleware to protect write operations to projectRoutes.js  (Relates #11)

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ProjectController = require('../controllers/Project.Controller');
+const requireApiKey = require('../middlewares/requireApiKey');
 
 // ===================================================================
 // RUTAS PARA PROYECTOS (Portfolio Showcase)
@@ -18,20 +19,20 @@ router.get('/', ProjectController.getAllProjects);
  * @desc    Crea un nuevo proyecto
  * @access  Public (Temporalmente)
  */
-router.post('/', ProjectController.createProject);
+router.post('/', requireApiKey, ProjectController.createProject);
 
 /**
  * @route   PATCH /api-resplandor/projects/:id
  * @desc    Actualiza parcialmente un proyecto
  * @access  Public (Temporalmente)
  */
-router.patch('/:id', ProjectController.updateProject);
+router.patch('/:id', requireApiKey, ProjectController.updateProject);
 
 /**
  * @route   DELETE /api-resplandor/projects/:id
  * @desc    Realiza un borrado lógico (Soft Delete)
  * @access  Public (Temporalmente)
  */
-router.delete('/:id', ProjectController.deleteProject);
+router.delete('/:id', requireApiKey, ProjectController.deleteProject);
 
 module.exports = router;
